f40461c74d3288bca6b56b544152179fdee17700
youngmoon <youngmoon@yunmi.local>
2015-05-11 13:58:38 +0900

    Hello Resume ?

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/helloresume-dev'
+  },
+
+  seedDB: true
+};
